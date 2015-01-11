29bf4d1c03c91502664ce3635b8f27c1a49ec399
AlasdairF <AlasdairF@users.noreply.github.com>
2015-01-11 16:52:09 +0700

    Added Len()

--- group.go
+++ group.go
@@ -3,6 +3,7 @@ package group
 type groupStruct struct {
  groups [][]int
  groupmap map[int]int
+ num int
 }
 
 func New() *groupStruct {
@@ -13,6 +14,10 @@ func New() *groupStruct {
 }
 
 func (g *groupStruct) Add(a ...int) {
+	
+	if len(a) == 0 {
+		return
+	}
 	
 	// Check how many groups already exist
 	var numgroups, grp, lastgroup, i, id int
@@ -35,6 +40,7 @@ func (g *groupStruct) Add(a ...int) {
 		for _, id = range a {
 			g.groupmap[id] = numgroups
 		}
+		num++
 		return
 	} else if numgroups == 1 {
 		// Add all to lastgroup
@@ -56,10 +62,13 @@ func (g *groupStruct) Add(a ...int) {
 		if grp == -1 {
 			newgroup = append(newgroup, id)
 		} else {
-			for _, id2 = range g.groups[grp] {
-				newgroup = append(newgroup, id2)
+			if len(g.groups[grp]) > 0 {
+				for _, id2 = range g.groups[grp] {
+					newgroup = append(newgroup, id2)
+				}
+				g.groups[grp] = make([]int, 0)
+				g.num--
 			}
-			g.groups[grp] = make([]int, 0)
 		}
 	}
 	grp = thisgroup[lastgroup]
@@ -67,6 +76,7 @@ func (g *groupStruct) Add(a ...int) {
 		g.groupmap[id] = grp
 	}
 	g.groups[grp] = newgroup
+	g.num++
 	return
 }
 
@@ -81,4 +91,8 @@ func (g *groupStruct) Groups() [][]int {
 		}
 	}
 	return groups[0:on]
+}
+
+func (g *groupStruct) Len() int {
+	return g.num
 }
